b8978b8ae97cb89dc28469138de0f178a8c08fe3
Emmanuel Adeniji <adenijiemma@gmail.com>
2022-12-13 12:30:59 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -32,15 +32,6 @@ void print_to_98(int n)
 		for (; n >= 98; n--)
 		{
 			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
 				
 		}
 	}
